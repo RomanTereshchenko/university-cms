f247be2b3c56f7057a4e6f6e9401d7c606a6b17a
Asus <Asus@DESKTOP-5NL9HPH>
2023-02-23 19:27:01 +0200

    Fail to get list of persons to the template.

--- university-cms/src/main/java/com/foxminded/javaspring/universitycms/controller/CourseController.java
+++ university-cms/src/main/java/com/foxminded/javaspring/universitycms/controller/CourseController.java
@@ -3,14 +3,15 @@ package com.foxminded.javaspring.universitycms.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.foxminded.javaspring.universitycms.dao.CourseDao;
 import com.foxminded.javaspring.universitycms.model.Course;
 
-@RestController
+@Controller
 @RequestMapping("/api/courses")
 public class CourseController {
 	
@@ -18,7 +19,7 @@ public class CourseController {
     private CourseDao courseDao;
 
     @GetMapping
-    public List<Course> findAll() {
+    public @ResponseBody List<Course> findAll() {
         return courseDao.findAll();
     }
 //
--- university-cms/src/main/java/com/foxminded/javaspring/universitycms/controller/PersonController.java
+++ university-cms/src/main/java/com/foxminded/javaspring/universitycms/controller/PersonController.java
@@ -3,25 +3,44 @@ package com.foxminded.javaspring.universitycms.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.foxminded.javaspring.universitycms.dao.PersonDao;
+import com.foxminded.javaspring.universitycms.dto.PersonDto;
 import com.foxminded.javaspring.universitycms.model.Person;
+import com.foxminded.javaspring.universitycms.service.PersonService;
 
-@RestController
+@Controller
 @RequestMapping("/api/persons")
 public class PersonController {
 	
-	// Create basic UI
-	
 	@Autowired
-    private PersonDao personDao;
+    private PersonService personService;
 
     @GetMapping
-    public List<Person> findAll() {
-        return personDao.findAll();
+    public @ResponseBody List<Person> findAll() {
+        return personService.findAllPersons();
+    }
+    
+    @GetMapping("/all")
+    public String showAll(Model model) {
+        model.addAttribute("persons", personService.findAllPersons());
+        return "persons/allPersons";
+    }
+    
+    @GetMapping("/create")
+    public String showCreateForm(Model model) {
+        PersonDto personsForm = new PersonDto();
+
+        for (int i = 1; i <= 3; i++) {
+            personsForm.addPerson(new Person());
+        }
+
+        model.addAttribute("form", personsForm);
+        return "persons/createPersonsForm";
     }
 
 }
