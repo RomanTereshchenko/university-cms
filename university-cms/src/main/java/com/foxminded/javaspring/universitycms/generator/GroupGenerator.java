2d5f1490bb7cfa6adf083d23f5b8bdd8d93e13f6
Asus <Asus@DESKTOP-5NL9HPH>
2023-03-03 07:26:33 +0200

    GroupGenerator, line 43

--- university-cms/src/main/java/com/foxminded/javaspring/universitycms/generator/GroupGenerator.java
+++ university-cms/src/main/java/com/foxminded/javaspring/universitycms/generator/GroupGenerator.java
@@ -3,12 +3,17 @@ package com.foxminded.javaspring.universitycms.generator;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.foxminded.javaspring.universitycms.dao.CourseDao;
 import com.foxminded.javaspring.universitycms.dao.GroupDao;
+import com.foxminded.javaspring.universitycms.model.Course;
 import com.foxminded.javaspring.universitycms.model.Group;
 
 import lombok.extern.slf4j.Slf4j;
@@ -20,11 +25,13 @@ public class GroupGenerator {
 
 	private Random random;
 	private GroupDao groupDao;
+	private CourseDao courseDao;
 
 	@Autowired
-	public GroupGenerator(Random random, GroupDao groupDao) {
+	public GroupGenerator(Random random, GroupDao groupDao, CourseDao courseDao) {
 		this.random = random;
 		this.groupDao = groupDao;
+		this.courseDao = courseDao;
 	}
 
 	@Transactional
@@ -33,11 +40,19 @@ public class GroupGenerator {
 		for (int i = 0; i < countToGenerate; i++) {
 			Group group = new Group();
 			group.setGroupName(generateGroupName());
+//			group.setCourses(getNRandomCourses(3));
 			groupDao.save(group);
 			groupsLocal.add(group);
 		}
 		log.info(countToGenerate + " groups generated");
 		return groupsLocal;
+	}
+
+	private Set<Course> getNRandomCourses(int numberOfCoursesInGroup) {
+		List<Course> courses = courseDao.findAll();
+
+		return IntStream.rangeClosed(1, numberOfCoursesInGroup)
+				.mapToObj(courseInGroupID -> courses.get(random.nextInt(courses.size()))).collect(Collectors.toSet());
 	}
 
 	private String generateGroupName() {
