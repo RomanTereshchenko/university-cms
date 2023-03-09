2733c0a7f749183a7529653722efcc2a86c731a9
Asus <Asus@DESKTOP-5NL9HPH>
2023-03-09 19:54:20 +0200

    Test

--- university-cms/src/test/java/com/foxminded/javaspring/universitycms/controllerTests/CourseControllerTest.java
+++ university-cms/src/test/java/com/foxminded/javaspring/universitycms/controllerTests/CourseControllerTest.java
@@ -4,12 +4,29 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.exceptions.base.MockitoException;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import com.foxminded.javaspring.universitycms.controller.CourseController;
 import com.foxminded.javaspring.universitycms.model.Course;
+import com.foxminded.javaspring.universitycms.service.CourseService;
 
-
+@RunWith(SpringRunner.class)
+@WebMvcTest(CourseController.class)
 public class CourseControllerTest {
+	
+	@Autowired
+    private MockMvc mvc;
+	
+	@MockBean
+	private CourseService courseService;
 
 	@Test
 	public void givenEmployees_whenGetEmployees_thenReturnJsonArray()
@@ -17,15 +34,15 @@ public class CourseControllerTest {
 	    
 	    Course course = new Course();
 
-	    List<Course> allCpurses = Arrays.asList(course);
+	    List<Course> allCourses = Arrays.asList(course);
 
-	    given(service.getAllEmployees()).willReturn(allEmployees);
+	    given(courseService.findAllCourses()).willReturn(allCourses);
 
-	    mvc.perform(get("/api/employees")
+	    mvc.perform(get("/api/courses")
 	      .contentType(MediaType.APPLICATION_JSON))
 	      .andExpect(status().isOk())
 	      .andExpect(jsonPath("$", hasSize(1)))
-	      .andExpect(jsonPath("$[0].name", is(alex.getName())));
+	      .andExpect(jsonPath("$[0].name", is(course.getCourseName())));
 	}
 	
 }
